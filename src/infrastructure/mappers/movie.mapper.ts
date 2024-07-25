7fae28669fa4e08d07c35d933513f3fe61cf6575
leandrogrzn <hekoej@gmail.com>
2024-07-25 18:36:55 -0500

    get movie - caso de uso

--- src/infrastructure/mappers/movie.mapper.ts
+++ src/infrastructure/mappers/movie.mapper.ts
@@ -1,5 +1,5 @@
-import { Movie } from "../../core/entities/movie.entity";
-import type { Result } from "../interfaces/movie-db.responses";
+import { FullMovie, Movie } from "../../core/entities/movie.entity";
+import type { MovieDBMovie, Result } from "../interfaces/movie-db.responses";
 
 export class MovieMapper {
 
@@ -12,6 +12,24 @@ export class MovieMapper {
       rating: result.vote_average,
       poster: `https://image.tmdb.org/t/p/w500${result.poster_path}`,
       backdrop: `https://image.tmdb.org/t/p/w500${result.backdrop_path}`
+    }
+  }
+
+  static fromMovieDBToEntity( movie: MovieDBMovie ): FullMovie {
+
+    return {
+      id: movie.id,
+      title: movie.title,
+      description: movie.overview,
+      releaseDate: new Date( movie.release_date),
+      rating: movie.vote_average,
+      poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+      generes: movie.genres.map( genre => genre.name),
+      duration: movie.runtime,
+      budget: movie.budget,
+      originalTitle: movie.original_title,
+      productionCopanies: movie.production_companies.map( company => company.name),
     }
   }
 
