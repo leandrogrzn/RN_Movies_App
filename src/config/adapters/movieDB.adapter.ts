c74e9384baee2d766eee7f7e5d9afe3bf965c853
leandrogrzn <hekoej@gmail.com>
2024-07-26 17:46:57 -0500

    variables de entorno rn

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,11 @@
+import { THE_MOVIE_DB_KEY } from "@env";
 import { AxiosAdapter } from "./http/axios.adapter";
 
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '4b632beb0fd1f0389f87a24da1869c76',
+    // api_key: '4b632beb0fd1f0389f87a24da1869c76',
+    api_key: THE_MOVIE_DB_KEY ?? 'No key',
     language: 'es'
   }
 })
