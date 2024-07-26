5e9d97898ba62411439ec11dbfe50beb19fc011e
leandrogrzn <hekoej@gmail.com>
2024-07-26 17:50:40 -0500

    full screen loader

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -5,6 +5,7 @@ import { useMovie } from "../../hooks/useMovie";
 import { MovieHeader } from "../../components/movie/MovieHeader";
 import { MovieDetails } from "../../components/movie/MovieDetails";
 import { ScrollView } from "react-native-gesture-handler";
+import { FullScreenLoader } from "../../components/loaders/FullScreenLoader";
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'>{};
 
@@ -14,8 +15,8 @@ export const DetailsScreen = ({route}: Props) => {
   const { isLoading, movie, cast = [] } = useMovie( movieId )
 
   if(isLoading){
-    return <Text>Loading</Text>
-  }
+    return(<FullScreenLoader />)
+  } 
 
   return (
     <ScrollView>
